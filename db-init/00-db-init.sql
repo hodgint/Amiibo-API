7f41d4ff2bf8f79bcc5d9d1264cc56a5dd2c7cc2
hodgint <hodgint@oregonstate.edu>
2019-04-24 17:08:20 -0700

    Finished getAmiibo and getAmiiboByID functions with correct functionality.

--- db-init/00-db-init.sql
+++ db-init/00-db-init.sql
@@ -8,13 +8,9 @@
 --
 
 
-DROP TABLE IF EXISTS `Amiibo`;
+DROP TABLE IF EXISTS `amiibo`;
 
-CREATE DATABASE amiiboapi;
-
-USE amiiboapi
-
-CREATE TABLE `Amiibo` (
+CREATE TABLE `amiibo` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `series` VARCHAR(255) NOT NULL,
@@ -26,7 +22,7 @@ CREATE TABLE `Amiibo` (
 );
 
 
-INSERT INTO Amiibo VALUES (1,'Mario', 'Super Smash Brothers', '11/21/2014', 'mariosmash.png','marioSSB.bin');
-INSERT INTO Amiibo VALUES (2,'peach', 'Super Smash Brothers', '11/21/2014', 'peachsmash.png','peachSSB.bin');
-INSERT INTO Amiibo VALUES (3,'Yoshi', 'Super Smash Brothers', '11/21/2014', 'yoshismash.png','yoshiSSB.bin');
-INSERT INTO Amiibo VALUES (4,'Donkey Kong', 'Super Smash Brothers', '11/21/2014', 'dksmash.png','dkSSB.bin');
+INSERT INTO amiibo VALUES (1,'Mario', 'Super Smash Brothers', '11/21/2014', 'mariosmash.png','marioSSB.bin');
+INSERT INTO amiibo VALUES (2,'peach', 'Super Smash Brothers', '11/21/2014', 'peachsmash.png','peachSSB.bin');
+INSERT INTO amiibo VALUES (3,'Yoshi', 'Super Smash Brothers', '11/21/2014', 'yoshismash.png','yoshiSSB.bin');
+INSERT INTO amiibo VALUES (4,'Donkey Kong', 'Super Smash Brothers', '11/21/2014', 'dksmash.png','dkSSB.bin');
--- main.js
+++ main.js
@@ -39,7 +39,7 @@ app.locals.mysqlPool = mysql.createPool({
 app.use(bodyParser.json());
 //app.use(express.static('public'));
 
-//app.use('/', api);
+app.use('/', api);
 
 
 app.use('*', function (req, res, next) {
